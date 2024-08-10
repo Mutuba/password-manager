39e215bae17e606de0f1025b99f2303b7c93ee93
Mutuba <danielmutubait@gmail.com>
2024-08-10 12:19:30 +0300

    Test expired token

--- spec/services/authorize_request_service_spec.rb
+++ spec/services/authorize_request_service_spec.rb
@@ -32,5 +32,16 @@ RSpec.describe AuthorizeRequestService, type: :service do
         expect { AuthorizeRequestService.call(**expired_token) }.to raise_error(JWT::ExpiredSignature, 'Signature has expired')
       end
     end
+
+    context 'when user is not found' do
+      before { user.destroy! }
+      it 'should raise decode token error' do
+        result = AuthorizeRequestService.call(**headers)
+        expect(result.user).to be_nil
+        expect(result.failure_message).to eq 'Invalid token'
+        expect(result.success?).to eq false
+        expect(result.failure?).to eq true
+      end
+    end
   end
 end
