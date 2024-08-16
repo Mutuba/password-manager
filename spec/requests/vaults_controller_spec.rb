a6aa43613b5a8f3d71ef88af3544be9b5b059f13
Mutuba <danielmutubait@gmail.com>
2024-08-16 17:02:36 +0300

    fix: Fix security of master password

--- spec/requests/vaults_controller_spec.rb
+++ spec/requests/vaults_controller_spec.rb
@@ -26,17 +26,34 @@ RSpec.describe VaultsController, type: :request do
   end
 
   describe '#login' do
-    context 'when user is authenticated' do
-      let(:vault) { build(:vault, user: ) }
+    let(:vault) { build(:vault, user:) }
+    let(:valid_password) { 'FavouritePassword123' }
+    let(:headers) { valid_headers(user.id) }
+
+    before do
+      vault.add_encrypted_master_key(valid_password)
+      vault.save!
+    end
+
+    context 'when correct password' do
       before do
-        vault.add_encrypted_master_key('Favouritepassword123')
-        vault.save!
-        post vault_login_path(vault.id), params: { master_password: 'Favouritepassword123' }.to_json, headers:
+        post vault_login_path(vault.id), params: { master_password: 'FavouritePassword123' }.to_json, headers:
       end
 
       it 'logs in' do
         expect(response).to have_http_status(:success)
         expect(json_response['message']).to eq 'Login successful'
+      end
+    end
+
+    context 'when wrong password' do
+      before do
+        post vault_login_path(vault.id), params: { master_password: 'Password1234' }.to_json, headers:
+      end
+
+      it 'does not login in' do
+        expect(response).to have_http_status(:unauthorized)
+        expect(json_response['error']).to eq 'Invalid password'
       end
     end
   end
