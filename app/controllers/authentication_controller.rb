209b72047ddfc4ba973d08d989c55ae4d64b5e44
Mutuba <danielmutubait@gmail.com>
2024-08-21 17:41:55 +0300

    fix: Authentication controller tests

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,10 +5,7 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :login
 
   def login
-    result = AuthenticateUserService.call(
-      username: login_params[:username],
-      password: login_params[:password]
-    )
+    result = AuthenticateUserService.call(username: login_params[:username], password: login_params[:password])
     raise AuthenticationError, Message.invalid_credentials unless result.success?
 
     json_response(auth_token: result.auth_token)
