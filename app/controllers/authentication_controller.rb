e2f5766c92b74cfa5b7138d789283ee05a0b3de6
Mutuba <danielmutubait@gmail.com>
2024-09-23 11:24:05 +0300

    fix: Ensure authorization before checking current user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AuthenticationController < ApplicationController
-  skip_before_action :authorize_request, only: [:login, :session]
+  skip_before_action :authorize_request, only: [:login]
 
   def login
     result = AuthenticateUserService.call(username: login_params[:username], password: login_params[:password])
