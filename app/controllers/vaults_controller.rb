684bac9100a2b4f8623ed43337241219584aa5cb
Mutuba <danielmutubait@gmail.com>
2024-09-27 11:01:57 +0300

    chore: Disable redis dependency

--- app/controllers/vaults_controller.rb
+++ app/controllers/vaults_controller.rb
@@ -36,8 +36,8 @@ class VaultsController < ApplicationController
       session_key = "vault:#{@vault.id}:user:#{current_user.id}"
       hashed_value = Digest::SHA256.hexdigest("authenticated#{current_user.id}")
 
-      session_timeout = ENV.fetch("SESSION_TIMEOUT", 10).to_i.minutes
-      REDIS.setex(session_key, session_timeout, hashed_value)
+      # session_timeout = ENV.fetch("SESSION_TIMEOUT", 10).to_i.minutes
+      # REDIS.setex(session_key, session_timeout, hashed_value)
 
       @vault.update(last_accessed_at: Time.current)
 
@@ -50,16 +50,16 @@ class VaultsController < ApplicationController
     end
   end
 
-  def logout
-    session_key = "vault:#{@vault.id}:user:#{current_user.id}"
+  # def logout
+  #   session_key = "vault:#{@vault.id}:user:#{current_user.id}"
 
-    if REDIS.exists?(session_key)
-      REDIS.del(session_key)
-      render(json: { message: "Logout successful" }, status: :ok)
-    else
-      render(json: { error: "No active session found" }, status: :unprocessable_entity)
-    end
-  end
+  #   if REDIS.exists?(session_key)
+  #     REDIS.del(session_key)
+  #     render(json: { message: "Logout successful" }, status: :ok)
+  #   else
+  #     render(json: { error: "No active session found" }, status: :unprocessable_entity)
+  #   end
+  # end
 
   private
 
